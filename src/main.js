a3fff43de63c01ad4cf1f9775b129daf23635558
fengjiahao66666 <2934182834@qq.com>
2023-02-15 21:42:27 +0800

    update-vuex

--- src/main.js
+++ src/main.js
@@ -4,14 +4,19 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入插件
 import VueResource from 'vue-resource' 
+//引入store
+import store from './store'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+
+
 //创建vm
 new Vue({
 	el:'#app',
 	render: h => h(App),
+	store,
 	beforeCreate(){
 		Vue.prototype.$bus = this
 	}
