3d85aa78f6a65890a6726bdb653b2c700ab3e504
fengjiahao66666 <2934182834@qq.com>
2023-02-16 13:29:59 +0800

    getters-qiuhe

---
+++ 25_src_求和案例_getters/main.js
@@ -0,0 +1,23 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+//引入插件
+import VueResource from 'vue-resource' 
+//引入store
+import store from './store'
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+
+Vue.use(VueResource)
+
+
+//创建vm
+new Vue({
+	el:'#app',
+	render: h => h(App),
+	store,
+	beforeCreate(){
+		Vue.prototype.$bus = this
+	}
+})
